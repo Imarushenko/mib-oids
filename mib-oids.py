c7693a4316335a466cacfd45ea0ca978c3ebaf2f
imarushchenko <igormarushenko@gmail.com>
2023-05-30 17:32:27 +0300

    extract oids from a mib file and write them to a csv file - refactoring

--- mib-oids.py
+++ mib-oids.py
@@ -8,7 +8,7 @@ csv_file_path = 'C:/develop/files/extracted-oids.csv'
 
 # read the MIB file
 with open(mib_file_path, 'r') as file:
-    mib_content = file.read()
+    mib_file_content = file.read()
 
 # regular expression pattern to match OIDs - sequence of digits separated by periods
 # every group of regex is related to the 3 variables below in the loop, '?: ...' - related to each variable
@@ -16,19 +16,19 @@ pattern = r"(?:(\S+)\s+OBJECT\s+IDENTIFIER\s+--\s*([\d.]+)\s*\n|(?:^|\n)\s*--\s*
 
 # find all matches of the pattern (OIDs) using re library, re.findall() function that finds all the matches in the
 # MIB re.MULTILINE search the matches on each line
-matches = re.findall(pattern, mib_content, re.MULTILINE)
+matches = re.findall(pattern, mib_file_content, re.MULTILINE)
 
 oids = []
 for match in matches:
     # define 3 variables to capture the values of corresponding groups from the regular expression match
-    # curr_oid = OBJECT IDENTIFIER in the MIB file
+    # oid = OBJECT IDENTIFIER in the MIB file
     # alternative_oid = -- in the MIB
-    # prev_oid = -- in the MIB inner lines
-    curr_oid, inner_oid, alternative_oid = match
-    if curr_oid:
-        oids.append(curr_oid)
-    elif inner_oid:
-        oids.append(inner_oid)
+    # other_regex_oid = -- in the MIB inner lines
+    other_regex_oid, oid, alternative_oid = match
+    if oid:
+        oids.append(oid)
+    elif other_regex_oid:
+        oids.append(other_regex_oid)
     elif alternative_oid:
         oids.append(alternative_oid)
 
@@ -40,4 +40,4 @@ with open(csv_file_path, 'w', newline='') as file:
 
 # prints
 print(f"All OIDs extracted from {mib_file_path} and saved to {csv_file_path} successfully!")
-print(f"Found {len(oids)} oids in {mib_file_path}.")
+print(f"OIDs list size: {len(oids)}")
