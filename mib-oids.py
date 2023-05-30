8c2db5ec3378dd238f2d28a1d0dc397bb82f79a1
imarushchenko <igormarushenko@gmail.com>
2023-05-30 17:20:30 +0300

    extract oids from a mib file and write them to a csv file - refactoring

--- mib-oids.py
+++ mib-oids.py
@@ -3,11 +3,11 @@ import csv
 import re   # a built-in module that provides support for regular expressions
 
 # define file paths
-mib_file = 'mib_file_path'
-csv_file = 'csv_file_path'
+mib_file_path = 'C:/develop/files/Frontline_ALLOT-MIB.mib'
+csv_file_path = 'C:/develop/files/extracted-oids.csv'
 
 # read the MIB file
-with open(mib_file, 'r') as file:
+with open(mib_file_path, 'r') as file:
     mib_content = file.read()
 
 # regular expression pattern to match OIDs - sequence of digits separated by periods
@@ -21,23 +21,23 @@ matches = re.findall(pattern, mib_content, re.MULTILINE)
 oids = []
 for match in matches:
     # define 3 variables to capture the values of corresponding groups from the regular expression match
-    # oid = OBJECT IDENTIFIER in the MIB file
+    # curr_oid = OBJECT IDENTIFIER in the MIB file
     # alternative_oid = -- in the MIB
-    # preceding_oid = -- in the MIB inner lines
-    preceding_oid, oid, alternative_oid = match
-    if oid:
-        oids.append(oid)
-    elif preceding_oid:
-        oids.append(preceding_oid)
+    # prev_oid = -- in the MIB inner lines
+    curr_oid, inner_oid, alternative_oid = match
+    if curr_oid:
+        oids.append(curr_oid)
+    elif inner_oid:
+        oids.append(inner_oid)
     elif alternative_oid:
         oids.append(alternative_oid)
 
 # write the OIDs to CSV file
-with open(csv_file, 'w', newline='') as file:
+with open(csv_file_path, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Object Identifier (OID)'])
     writer.writerows([(oid,) for oid in oids])
 
 # prints
-print(f"All OIDs extracted from {mib_file} and saved to {csv_file} successfully!")
-print(f"OIDs list size: {len(oids)}")
+print(f"All OIDs extracted from {mib_file_path} and saved to {csv_file_path} successfully!")
+print(f"Found {len(oids)} oids in {mib_file_path}.")
